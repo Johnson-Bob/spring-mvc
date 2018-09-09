281a3ea4f5e92e7bd05598eebc9a40a21df8dfc0
Johnson-Bob <gladius76@ukr.net>
2018-09-09 13:10:14 +0300

    Lecture 32 ListingProducts

---
+++ src/main/java/com/spring4/core/springmvc/services/ProductService.java
@@ -0,0 +1,9 @@
+package com.spring4.core.springmvc.services;
+
+import com.spring4.core.springmvc.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAllProducts();
+}
