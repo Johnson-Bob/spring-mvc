35db7326c29782fe4a96deb5ca3718ff777c76f6
Johnson-Bob <gladius76@ukr.net>
2018-10-07 14:59:42 +0300

    Lecture 34 Create a New Product

--- src/main/java/com/spring4/core/springmvc/controllers/ProductController.java
+++ src/main/java/com/spring4/core/springmvc/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.spring4.core.springmvc.controllers;
 
+import com.spring4.core.springmvc.domain.Product;
 import com.spring4.core.springmvc.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProductController {
@@ -26,5 +28,17 @@ public class ProductController {
     public String getProduct(@PathVariable Integer id, Model model) {
         model.addAttribute("product", productService.getProductById(id));
         return "product";
+    }
+
+    @RequestMapping("/product/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "productForm";
+    }
+
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    public String saveOrUpdateProduct(Product product) {
+        Product savedProduct = productService.saveOrUpdateProduct(product);
+        return "redirect:/product/" + savedProduct.getId();
     }
 }
