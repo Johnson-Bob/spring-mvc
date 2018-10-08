91c9071c91d1f9818385db3de568e58409bed0ad
Johnson-Bob <gladius76@ukr.net>
2018-10-08 06:07:53 +0300

    Lecture 37 Create Customer HTMLs and Controller

---
+++ src/main/java/com/spring4/core/springmvc/controllers/CustomerController.java
@@ -0,0 +1,57 @@
+package com.spring4.core.springmvc.controllers;
+
+import com.spring4.core.springmvc.domain.Customer;
+import com.spring4.core.springmvc.domain.Product;
+import com.spring4.core.springmvc.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CustomerController {
+    private CustomerService customerService;
+
+    @Autowired
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping("/customers")
+    public String listCustomers(Model model) {
+        model.addAttribute("customers", customerService.listAllCustomers());
+        return "customer/customers";
+    }
+
+    @RequestMapping("/customer/{id}")
+    public String getCustomer(@PathVariable Integer id, Model model) {
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        return "customer/customer";
+    }
+
+    @RequestMapping("/customer/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        return "customer/customerForm";
+    }
+
+    @RequestMapping("/customer/new")
+    public String newCustomer(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "customer/customerForm";
+    }
+
+    @RequestMapping(value = "/customer", method = RequestMethod.POST)
+    public String saveOrUpdateCustomer(Customer customer) {
+        Customer savedCustomer = customerService.saveOrUpdateCustomer(customer);
+        return "redirect:/customer/" + savedCustomer.getId();
+    }
+
+    @RequestMapping("/customer/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        customerService.delete(id);
+        return "redirect:/customers/";
+    }
+}
--- src/main/java/com/spring4/core/springmvc/services/CustomerServiceImpl.java
+++ src/main/java/com/spring4/core/springmvc/services/CustomerServiceImpl.java
@@ -1,11 +1,17 @@
 package com.spring4.core.springmvc.services;
 
 import com.spring4.core.springmvc.domain.Customer;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
     private Map<Integer, Customer> customers;
+
+    public CustomerServiceImpl() {
+        loadCustomers();
+    }
 
     @Override
     public List<Customer> listAllCustomers() {
@@ -37,6 +43,43 @@ public class CustomerServiceImpl implements CustomerService {
 
     private void loadCustomers() {
         customers = new HashMap<>();
+
+        Customer customer1 = new Customer();
+        customer1.setId(1);
+        customer1.setFirstName("Micheal");
+        customer1.setLastName("Weston");
+        customer1.setAddressLineOne("1 Main St");
+        customer1.setCity("Miami");
+        customer1.setState("Florida");
+        customer1.setZipCode("33101");
+        customer1.setEmail("micheal@burnnotice.com");
+        customer1.setPhoneNumber("305.333.0101");
+
+        Customer customer2 = new Customer();
+        customer2.setId(2);
+        customer2.setFirstName("Fiona");
+        customer2.setLastName("Glenanne");
+        customer2.setAddressLineOne("1 Key Biscane Ave");
+        customer2.setCity("Miami");
+        customer2.setState("Florida");
+        customer2.setZipCode("33101");
+        customer2.setEmail("fiona@burnnotice.com");
+        customer2.setPhoneNumber("305.323.0233");
+
+        Customer customer3 = new Customer();
+        customer3.setId(3);
+        customer3.setFirstName("Sam");
+        customer3.setLastName("Axe");
+        customer3.setAddressLineOne("1 Little Cuba Road");
+        customer3.setCity("Miami");
+        customer3.setState("Florida");
+        customer3.setZipCode("33101");
+        customer3.setEmail("sam@burnnotice.com");
+        customer3.setPhoneNumber("305.426.9832");
+
+        customers.put(1, customer1);
+        customers.put(2, customer2);
+        customers.put(3, customer3);
     }
 
     private Integer getNextId() {
