59c7c8a62af926e810ec980efea7be55a2ef6978
vudovenko <v.udovenko@petrosoftinc.com>
2018-10-09 18:50:39 +0300

    Lecture 48 JPA Entities

--- src/main/java/com/spring4/core/springmvc/domain/Product.java
+++ src/main/java/com/spring4/core/springmvc/domain/Product.java
@@ -1,9 +1,18 @@
 package com.spring4.core.springmvc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class Product implements DomainObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String description;
     private BigDecimal price;
     private String imageUrl;
