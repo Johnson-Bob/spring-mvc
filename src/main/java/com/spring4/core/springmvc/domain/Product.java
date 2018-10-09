c54235a670ae6ddc7e1eac70f178d760218c587d
vudovenko <v.udovenko@petrosoftinc.com>
2018-10-09 18:59:00 +0300

    Lecture 48 JPA Optimistic Locking

--- src/main/java/com/spring4/core/springmvc/domain/Product.java
+++ src/main/java/com/spring4/core/springmvc/domain/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 import java.math.BigDecimal;
 
 @Entity
@@ -12,6 +13,9 @@ public class Product implements DomainObject {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Version
+    private Integer version;
 
     private String description;
     private BigDecimal price;
@@ -25,6 +29,14 @@ public class Product implements DomainObject {
     @Override
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 
     public String getDescription() {
