0516f2452acda4c8efdf3dfd1324dd6893ac0ef6
vudovenko <v.udovenko@petrosoftinc.com>
2018-10-09 19:13:14 +0300

    Lecture 50 JPA Customer Entity

--- src/main/java/com/spring4/core/springmvc/services/CustomerServiceImpl.java
+++ src/main/java/com/spring4/core/springmvc/services/CustomerServiceImpl.java
@@ -13,7 +13,7 @@ public class CustomerServiceImpl extends AbstractMapService<Customer> implements
         customer1.setId(1);
         customer1.setFirstName("Micheal");
         customer1.setLastName("Weston");
-        customer1.setAddressLineOne("1 Main St");
+        customer1.setAddressLine1("1 Main St");
         customer1.setCity("Miami");
         customer1.setState("Florida");
         customer1.setZipCode("33101");
@@ -24,7 +24,7 @@ public class CustomerServiceImpl extends AbstractMapService<Customer> implements
         customer2.setId(2);
         customer2.setFirstName("Fiona");
         customer2.setLastName("Glenanne");
-        customer2.setAddressLineOne("1 Key Biscane Ave");
+        customer2.setAddressLine1("1 Key Biscane Ave");
         customer2.setCity("Miami");
         customer2.setState("Florida");
         customer2.setZipCode("33101");
@@ -35,7 +35,7 @@ public class CustomerServiceImpl extends AbstractMapService<Customer> implements
         customer3.setId(3);
         customer3.setFirstName("Sam");
         customer3.setLastName("Axe");
-        customer3.setAddressLineOne("1 Little Cuba Road");
+        customer3.setAddressLine1("1 Little Cuba Road");
         customer3.setCity("Miami");
         customer3.setState("Florida");
         customer3.setZipCode("33101");
