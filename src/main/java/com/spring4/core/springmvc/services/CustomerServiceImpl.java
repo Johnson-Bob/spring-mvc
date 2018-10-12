45b98a83a4f2070223c803493a8b44bea83e9a71
Johnson-Bob <gladius76@ukr.net>
2018-10-12 09:16:23 +0300

    Lecture 54 Create Customer DAO and Tests

--- src/main/java/com/spring4/core/springmvc/services/CustomerServiceImpl.java
+++ src/main/java/com/spring4/core/springmvc/services/CustomerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.spring4.core.springmvc.services;
 
 import com.spring4.core.springmvc.domain.Customer;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("map")
 public class CustomerServiceImpl extends AbstractMapService<Customer> implements CustomerService {
 
 }
